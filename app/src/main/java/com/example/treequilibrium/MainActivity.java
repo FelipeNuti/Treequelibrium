1c3f2064cdbfbb7aa730e26e8a2d33011c3b079f
FelipeNuti <felipenuti1182@gmail.com>
2020-11-14 17:25:50 +0000

    added circular progress bar

--- app/src/main/java/com/example/treequilibrium/MainActivity.java
+++ app/src/main/java/com/example/treequilibrium/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.treequilibrium;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    Button buttonDonate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
